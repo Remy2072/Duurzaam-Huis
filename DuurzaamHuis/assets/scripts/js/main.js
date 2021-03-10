478e9f1fd920eeafdb874e47e58801973558e99b
Remy2072 <30528@ma-web.nl>
2021-03-10 15:57:23 +0100

    de hele pagina opnieuw gedesigned

--- DuurzaamHuis/assets/scripts/js/main.js
+++ DuurzaamHuis/assets/scripts/js/main.js
@@ -1,4 +1,26 @@
-// DARKMODE BUTTON -----------------------------------------------------
+const menuIconEl = $('.menu-icon');
+const sidenavEl = $('.sidenav');
+const sidenavCloseEl = $('.sidenav__close-icon');
+
+// Add and remove provided class names
+function toggleClassName(el, className) {
+  if (el.hasClass(className)) {
+    el.removeClass(className);
+  } else {
+    el.addClass(className);
+  }
+}
+
+// Open the side nav on click
+menuIconEl.on('click', function() {
+  toggleClassName(sidenavEl, 'active');
+});
+
+// Close the side nav on click
+sidenavCloseEl.on('click', function() {
+  toggleClassName(sidenavEl, 'active');
+});
+
 var toggleDarkmode = document.querySelector(".toggle-btn input");
 
 // console.log(toggleDarkmode);
@@ -7,8 +29,8 @@ toggleDarkmode.addEventListener("click", toggleDarkmodeAppearance);
 
 function toggleDarkmodeAppearance() {
   if (toggleDarkmode.checked == true) {
-    document.querySelector("body").classList.add("darkmode");
+    document.querySelector("body").classList.remove("lightmode");
   } else {
-    document.querySelector("body").classList.remove("darkmode");
+    document.querySelector("body").classList.add("lightmode");
   }
 }
