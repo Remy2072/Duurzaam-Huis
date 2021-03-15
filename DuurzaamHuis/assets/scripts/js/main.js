e04d2c5fbf2b06608d08e4d181814723bad90ce3
Kaan Secen <24114@ma-web.nl>
2021-03-15 12:49:41 +0100

    Tijd toegevoegd

--- DuurzaamHuis/assets/scripts/js/main.js
+++ DuurzaamHuis/assets/scripts/js/main.js
@@ -34,3 +34,86 @@ function toggleDarkmodeAppearance() {
     document.querySelector("body").classList.add("lightmode");
   }
 }
+
+
+// Tijd
+
+function start () {
+  setInterval(function () {
+    let time = new Date();
+    let sec = time.getSeconds();
+    let min = time.getMinutes();
+    let hr = time.getHours();
+    let day = " AM";
+    if (hr > 12) {
+      day = " PM";
+      hr = hr - 12;
+    }
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+    if (min < 10) {
+      min = "0" + min;
+    }
+    if (hr < 10) {
+      hr = "0" + hr;
+    }
+    clockdisplay(hr, min, sec, day);
+  }, 100);
+  
+  function clockdisplay (hr, min, sec, day){
+    let clock = document.getElementById("display");
+    clock.innerHTML = hr + ":" + min + ":" + sec + "" + day;
+  }
+  
+  var now = new Date();
+  
+  var days = new Array(
+    "Zondag",
+    "Maandag",
+    "Dinsdag",
+    "Woensdag",
+    "Donderdag",
+    "Vrijdag",
+    "Zaterdag"
+  );
+  
+  var months = new Array(
+    "Januari",
+    "Februari",
+    "Maart",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Augustus",
+    "September",
+    "Oktober",
+    "November",
+    "December"
+  );
+  
+  var date = (now.getDate() < 10 ? "0" : "") + now.getDate();
+  
+  function fourdigits(number) {
+    return number < 1000 ? number + 1900 : number;
+  }
+  today =
+    days[now.getDay()] +
+    " " +
+    months[now.getMonth()] +
+    " " +
+    date +
+    " " +
+    fourdigits(now.getYear());
+  
+  console.log(today);
+  var dag = document.getElementById("dagmaandjaar");
+  dag.innerHTML = today;
+  var place = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  plek = document.getElementById("places");
+  plek.innerHTML = place;
+  
+}
+
+window.addEventListener('DOMContentLoaded', start);
