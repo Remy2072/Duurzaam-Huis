c5fd0fc175ebb566157157e5c44b842513fcfc7a
Remy2072 <30528@ma-web.nl>
2021-03-30 21:41:32 +0200

    grafiek gefixt

--- DuurzaamHuis/assets/scripts/main.js
+++ DuurzaamHuis/assets/scripts/main.js
@@ -23,12 +23,15 @@ sidenavCloseEl.on("click", function () {
 var toggleDarkmode = document.querySelector(".toggle-btn input");
 
 toggleDarkmode.addEventListener("click", toggleDarkmodeAppearance);
-
+var lijnkleur = "#fff";
 function toggleDarkmodeAppearance() {
+  grafiek();
   if (toggleDarkmode.checked == true) {
     document.querySelector("body").classList.remove("lightmode");
+    lijnkleur = "#fff";
   } else {
     document.querySelector("body").classList.add("lightmode");
+    lijnkleur = "#2d2d2d";
   }
 }
 
@@ -98,15 +101,17 @@ thermostaat.addEventListener("change", () => {
 });
 
 // *SPOTIFY ---------------------------------------------------------
-document.getElementById("playAudio").addEventListener("click", function(){
-	var audio = document.getElementById('testAudio');
-  if(this.className == 'is-playing'){
+document.getElementById("playAudio").addEventListener("click", function () {
+  var audio = document.getElementById("testAudio");
+  if (this.className == "is-playing") {
     this.className = "";
-    this.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 111.87 111.87"><defs><style>.cls-1{fill:#fff;}</style></defs><g id="Laag_2" data-name="Laag 2"><g id="Laag_1-2" data-name="Laag 1"><path class="cls-1" d="M55.94,0a55.94,55.94,0,1,0,55.93,55.94A55.93,55.93,0,0,0,55.94,0ZM75.71,57.51,44.87,75.34a1.82,1.82,0,0,1-2.73-1.58V38.11a1.82,1.82,0,0,1,2.73-1.58L75.71,54.36A1.82,1.82,0,0,1,75.71,57.51Z"></path></g></g></svg>'
+    this.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 111.87 111.87"><defs><style>.cls-1{fill:#fff;}</style></defs><g id="Laag_2" data-name="Laag 2"><g id="Laag_1-2" data-name="Laag 1"><path class="cls-1" d="M55.94,0a55.94,55.94,0,1,0,55.93,55.94A55.93,55.93,0,0,0,55.94,0ZM75.71,57.51,44.87,75.34a1.82,1.82,0,0,1-2.73-1.58V38.11a1.82,1.82,0,0,1,2.73-1.58L75.71,54.36A1.82,1.82,0,0,1,75.71,57.51Z"></path></g></g></svg>';
     audio.pause();
-  }else{
+  } else {
     this.className = "is-playing";
-    this.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 111.87 111.87"><defs><style>.cls-1{fill:#fff;}</style></defs><g id="Laag_2" data-name="Laag 2"><g id="Laag_1-2" data-name="Laag 1"><path class="cls-1" d="M55.94,0a55.94,55.94,0,1,0,55.93,55.94A55.93,55.93,0,0,0,55.94,0ZM48.87,74.69a1.75,1.75,0,0,1-1.75,1.75h-6.6a1.75,1.75,0,0,1-1.75-1.75V37.92a1.75,1.75,0,0,1,1.75-1.75h6.6a1.75,1.75,0,0,1,1.75,1.75Zm24.23,0a1.75,1.75,0,0,1-1.75,1.75h-6.6A1.75,1.75,0,0,1,63,74.69V37.92a1.75,1.75,0,0,1,1.75-1.75h6.6a1.75,1.75,0,0,1,1.75,1.75Z"></path></g></g></svg>';
+    this.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 111.87 111.87"><defs><style>.cls-1{fill:#fff;}</style></defs><g id="Laag_2" data-name="Laag 2"><g id="Laag_1-2" data-name="Laag 1"><path class="cls-1" d="M55.94,0a55.94,55.94,0,1,0,55.93,55.94A55.93,55.93,0,0,0,55.94,0ZM48.87,74.69a1.75,1.75,0,0,1-1.75,1.75h-6.6a1.75,1.75,0,0,1-1.75-1.75V37.92a1.75,1.75,0,0,1,1.75-1.75h6.6a1.75,1.75,0,0,1,1.75,1.75Zm24.23,0a1.75,1.75,0,0,1-1.75,1.75h-6.6A1.75,1.75,0,0,1,63,74.69V37.92a1.75,1.75,0,0,1,1.75-1.75h6.6a1.75,1.75,0,0,1,1.75,1.75Z"></path></g></g></svg>';
     audio.play();
   }
 });
@@ -260,68 +265,69 @@ if (mq.matches) {
   inputtodo.setAttribute("maxlength", "18");
 }
 
-
 // *GRAFIEK ----------------------------------------------------------
 
+function grafiek() {
+  var xmlhttp = new XMLHttpRequest();
+  var url = "assets/scripts/jsonData.json";
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.responseText);
 
-var xmlhttp = new XMLHttpRequest();
-var url = "assets/scripts/jsonData.json";
-xmlhttp.open("GET",url, true);
-xmlhttp.send();
-xmlhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-        var data = JSON.parse(this.responseText);
+      var months = data.months_temperature.map(function (elem) {
+        return elem.date;
+      });
 
-        var months = data.months_temperature.map(function(elem) {
-            return elem.date;
-        });
-        
-        var avg = data.months_temperature.map(function(elem) {
-            return elem.avg;
-        });
-        var your = data.months_temperature.map(function(elem) {
-            return elem.your;
-        });
-
-        
-        var ctx = document.getElementById('canvas').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: months,
-                datasets: [{
-                    label: 'Gemiddeld watergebruik',
-                    data: avg,
-                    backgroundColor: 'transparent',
-                    borderColor: '#fff',
-                    borderWidth: 3
-                },
-                {
-                    label: 'Jouw watergebruik',
-                    data: your,
-                    backgroundColor: 'transparent',
-                    borderColor: '#17c9b7',
-                    borderWidth: 3
-                }]
+      var avg = data.months_temperature.map(function (elem) {
+        return elem.avg;
+      });
+      var your = data.months_temperature.map(function (elem) {
+        return elem.your;
+      });
+      var ctx = document.getElementById("canvas").getContext("2d");
+      var myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: months,
+          datasets: [
+            {
+              label: "Gemiddeld watergebruik",
+              data: avg,
+              backgroundColor: "transparent",
+              borderColor: lijnkleur,
+              borderWidth: 3,
             },
-            options: {
-              maintainAspectRatio: false,
-              responsive: true,
-                elements:{
-                    line:{
-                        tension:0
-                    }
+            {
+              label: "Jouw watergebruik",
+              data: your,
+              backgroundColor: "transparent",
+              borderColor: "#17c9b7",
+              borderWidth: 3,
+            },
+          ],
+        },
+        options: {
+          maintainAspectRatio: false,
+          responsive: true,
+          elements: {
+            line: {
+              tension: 0,
+            },
+          },
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
                 },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-
+              },
+            ],
+          },
+        },
+      });
     }
-
+  };
 }
+grafiek();
