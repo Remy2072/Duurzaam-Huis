0e097d12a2c1ace2f8fdfb2b01e59f911233e828
Jaymar Westerlow <jaywesterlow71@gmail.com>
2021-03-30 18:48:31 +0200

    Grafiek added

--- DuurzaamHuis/assets/scripts/main.js
+++ DuurzaamHuis/assets/scripts/main.js
@@ -261,3 +261,65 @@ if (mq.matches) {
 }
 
 
+// *GRAFIEK ----------------------------------------------------------
+
+
+var xmlhttp = new XMLHttpRequest();
+var url = "http://localhost/DuurzaamHuis/assets/scripts/jsonData.json";
+xmlhttp.open("GET",url, true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        var data = JSON.parse(this.responseText);
+
+        var months = data.months_temperature.map(function(elem) {
+            return elem.date;
+        });
+        
+        var avg = data.months_temperature.map(function(elem) {
+            return elem.avg;
+        });
+        var your = data.months_temperature.map(function(elem) {
+            return elem.your;
+        });
+
+        
+        var ctx = document.getElementById('canvas').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: months,
+                datasets: [{
+                    label: 'Gemiddeld watergebruik',
+                    data: avg,
+                    backgroundColor: 'transparent',
+                    borderColor: '#fff',
+                    borderWidth: 3
+                },
+                {
+                    label: 'Jouw watergebruik',
+                    data: your,
+                    backgroundColor: 'transparent',
+                    borderColor: '#17c9b7',
+                    borderWidth: 3
+                }]
+            },
+            options: {
+                elements:{
+                    line:{
+                        tension:0
+                    }
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+    }
+
+}
